af74815c46f6d0c740b95c6f39c51a3ab271f11d
Gbolaww <154886840+Gbolaww@users.noreply.github.com>
2025-10-05 22:14:36 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Banner from "./Banner";
+import About from "./About ";
+import Services from "./Services";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <div>
+      <h1>My Basic React SPA</h1>
+      <Banner />
+      <About />
+      <Services />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
